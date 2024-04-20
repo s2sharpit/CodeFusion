252cdaba3606b02c6d10df591e6e3af5f86f36fe
Tushar Saini <s2sharpit@gmail.com>
2024-04-21 03:27:05 +0530

    Refactor DevsLoading component to display 4 loading items instead of 3
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/components/suspense/DevsLoading.tsx
+++ src/components/suspense/DevsLoading.tsx
@@ -4,14 +4,14 @@ import { Wrapper } from "@/components/ui";
 export default function DevsLoading() {
   return (
     <Wrapper variant="flex">
-      {[...Array(3)].map((_, k) => (
+      {[...Array(4)].map((_, k) => (
         <div
           key={k}
-          className="p-2 border border-gray-800 rounded-md w-40 animate-pulse grid place-items-center"
+          className="p-4 border border-gray-800 rounded-md w-40 animate-pulse grid place-items-center gap-4"
         >
-          <div className="w-full h-4 bg-gray-700 rounded mb-2"></div>
-          <div className="w-24 h-24 bg-gray-700 rounded-full mb-2"></div>
-          <div className="w-full h-4 bg-gray-700 rounded mb-2"></div>
+          <div className="w-full h-4 bg-gray-700 rounded"></div>
+          <div className="w-24 h-24 bg-gray-700 rounded-full"></div>
+          <div className="w-full h-4 bg-gray-700 rounded"></div>
           {/* <div className="w-full h-12 bg-gray-700 rounded"></div> */}
         </div>
       ))}
