108de966b7d407b90d7d484052dcdde2fb6eae2f
Tushar Saini <s2sharpit@gmail.com>
2024-04-21 03:09:16 +0530

    Refactor Home component to use Section and Title components
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import Image from "next/image";
-import { auth } from "@/lib/auth";
+import { Section, Title } from "@/components/ui";
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="font-bold text-3xl text-green-600 pb-4">Home</h1>
-        <Image
-          className="border border-green-500 rounded-full"
-          src={session?.user.image as string}
-          alt={session?.user.username as string}
-          width={200}
-          height={200}
-        />
-        <h1>@{session?.user.username}</h1>
-        <p>{session?.user.name}</p>
-      </div>
-    </main>
+    <Section>
+      <Title className="min-h-screen">
+        Code<span className="text-primary">Fusion</span>
+      </Title>
+    </Section>
   );
 }
